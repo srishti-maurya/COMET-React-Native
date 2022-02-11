d10c26bc09b5250dc9e9b748c242d2871711a39a
srishti-maurya <39724354+srishti-maurya@users.noreply.github.com>
2022-02-11 14:06:15 +0530

    first commit

---
+++ Demo/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Config from "react-native-config";
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView >
+      <ScrollView>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>Hello world</Text>
+          <Text>My api url : {Config.API_URL}</Text>
+        <Text>{Config.GOOGLE_MAPS_API_KEY}</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
